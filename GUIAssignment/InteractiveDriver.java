315ecf25b4e301f6672d1968bb780d8a3cff4b89
Tonia Jackson <tonia.julie@gmail.com>
2014-05-20 14:21:04 +0100

    Create InteractiveDriver.java

---
+++ GUIAssignment/InteractiveDriver.java
@@ -0,0 +1,11 @@
+package GUIAssignment;
+
+public class InteractiveDriver
+{
+  public static void main(String[] args)
+  {
+    InteractiveFrame IF = new InteractiveFrame();
+    IF.showIt();
+  }
+
+}
